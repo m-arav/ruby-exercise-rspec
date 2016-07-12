d9f276d3b0035bd05e2b9c3b69926389dc370b5f
Aravind M <darthvader@Qs-iMac-2.local>
2016-07-12 16:41:28 +0530

    Add test for user model

--- lib/user_model.rb
+++ lib/user_model.rb
@@ -1,5 +1,10 @@
-# User info
+# Contains information about the Data Model to be used
 class UserModel
+  attr_accessor :user_name, :user_city, :user_age
+
   def initialize(name, city, age)
+    @user_name = name
+    @user_city = city
+    @user_age = age
   end
 end
--- spec/user_model_spec.rb
+++ spec/user_model_spec.rb
@@ -2,8 +2,17 @@ require 'user_model'
 describe UserModel do
   describe '#initilize' do
     context 'when initialized' do
-      it 'should recieve arguments' do
-        expect(UserModel.new).to raise_error
+      it 'should raise error if 0 arg passed' do
+        expect { UserModel.new }.to raise_error ArgumentError
+      end
+      it 'should raise error if 1 arg passed' do
+        expect { UserModel.new(1) }.to raise_error ArgumentError
+      end
+      it 'should raise error if 2 arg passed' do
+        expect { UserModel.new(1, 2) }.to raise_error ArgumentError
+      end
+      it 'should pass when 3 arg passed' do
+        expect { UserModel.new(1, 2, 3) }.to_not raise_error
       end
     end
   end
